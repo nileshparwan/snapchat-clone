2e25ad6dc4b197fcd8ea0df328a524578cd40f92
Koshal Parwan <Koshal.Parwan@hangarww.com>
2021-01-17 14:50:54 +0400

    Add Camera + Camera state on redux end

--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -1,17 +1,33 @@
-import React, {useRef} from 'react'; 
+import React, { useRef, useCallback } from 'react';
+import {useDispatch} from 'react-redux';
 import Webcam from 'react-webcam';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import { setCameraImage } from './features/cameraSlice';
+import { useHistory } from 'react-router-dom';
+import './WebcamCapture.css'; 
 
 const videoConstraints = {
     width: 250,
     height: 400,
     facingMode: "user",
-}
+};
 
 function WebcamCapture() {
-    const webCamRef = useRef(null)
+    const webCamRef = useRef(null);
+    const dispatch = useDispatch(); // allows us to add data to redux
+    const history = useHistory();
+
+    const capture = useCallback(() => {
+        // use callbasck will run the function once and will same the output
+        // when dependancy changes it re renders the function {webcamref}
+        const imageSrc = webCamRef?.current.getScreenshot();
+        dispatch(setCameraImage(imageSrc)); 
+        history.push('/preview'); 
+    }, [webCamRef]);
+
     return (
         <div className="webcamCapture">
-            <Webcam 
+            <Webcam
                 audio={false}
                 height={videoConstraints.height}
                 ref={webCamRef}
@@ -19,8 +35,15 @@ function WebcamCapture() {
                 width={videoConstraints.width}
                 videoConstraints={videoConstraints}
             />
+
+            <RadioButtonUncheckedIcon
+                className="webcamCapture__button"
+                onClick={capture}
+                fontSize="large"
+            />
+
         </div>
-    )
+    );
 }
 
-export default WebcamCapture
+export default WebcamCapture;
--- src/features/cameraSlice.js
+++ src/features/cameraSlice.js
@@ -12,18 +12,11 @@ export const cameraSlice = createSlice({
         resetCameraImage: (state) => {
             state.cameraImage = null;
         },
-
     },
 });
 
 export const { setCameraImage, resetCameraImage } = cameraSlice.actions;
 
-
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectcamera = state => state.camera.value;
-
-export const selectCameraImage = (state) => state.camera.cameraImage; 
+export const selectCameraImage = (state) => state.camera.cameraImage; //allows us to pull data from redux
 
 export default cameraSlice.reducer;
